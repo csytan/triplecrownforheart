21c337c7c52bd10a17aca5805b97bdc14f69c294
Chris Tan <csytan@gmail.com>
2015-06-25 02:23:37 -0400

    script for server

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while true
+do
+    git pull
+    python3 run.py
+    git add donations.json
+    git add riders.json
+    git commit -m 'auto update'
+    git push
+    sleep 5
+done
